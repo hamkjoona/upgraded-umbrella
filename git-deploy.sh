3af40e0db2179be0d4272db9bc0a256fbbccb943
Seppo <a.b@c>
2022-12-16 16:47:33 +0200

    Lisätty commit-viestin vaatimus.

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+# Ei ota kantaa tunnistukseen, ssh-avaimen ja ssh-add -komennon käyttöä suositellaan pusksemisen sujuvoittamiseksi. 
+
 git add .
 
 git_status=$(git status --porcelain)
 has_changes=$(echo $git_status| wc -l)
 
 if [ $has_changes ]; then
-	echo Komminoitavia muutoksia:
+	echo Komminoitavat tiedostot:
 	echo $git_status
 	echo
 	echo Anna commit-viesti:
 	read viesti
 
+	if [ -z $viesti ]; then
+		echo Viesti on tyhjä!
+		exit	
+	fi
+
 	print $viesti;
 
 	git commit -am "$viesti"
 else 
-	Ei muutoksia.
+	echo Ei muutoksia.
 fi
 
 echo "Pusketaanko? (k/e)"
